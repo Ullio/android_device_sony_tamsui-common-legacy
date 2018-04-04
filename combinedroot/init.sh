67deebb7cce009b24bbe8c4325551a4de0e8d0a6
Olliu <ulli_for@gmail.com>
2018-04-04 21:28:30 +0200

    Ullio: revert temporary changes back and use TamsuiCM11 version - apart from the green LES
    Change-Id: I531c4d3e8634ed22b8237a5eb793a340902dc6fc

--- combinedroot/init.sh
+++ combinedroot/init.sh
@@ -1,4 +1,18 @@
 #!/sbin/busybox sh
+# Copyright (C) 2012-2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
 set +x
 _PATH="$PATH"
 export PATH=/sbin
@@ -17,13 +31,9 @@ busybox mkdir -m 755 -p /dev/block
 busybox mkdir -m 755 -p /dev/input
 busybox mkdir -m 555 -p /proc
 busybox mkdir -m 755 -p /sys
-busybox mkdir -m 755 -p /system
-busybox mkdir -m 755 -p /data
 
 # create device nodes
 busybox mknod -m 600 /dev/block/mmcblk0 b 179 0
-busybox mknod -m 600 /dev/block/mmcblk0p10 b 179 10
-busybox mknod -m 600 /dev/block/mmcblk0p15 b 179 15
 busybox mknod -m 600 ${BOOTREC_CACHE_NODE}
 busybox mknod -m 600 ${BOOTREC_EVENT_NODE}
 busybox mknod -m 666 /dev/null c 1 3
@@ -32,8 +42,6 @@ busybox mknod -m 666 /dev/null c 1 3
 busybox mount -t proc proc /proc
 busybox mount -t sysfs sysfs /sys
 busybox mount -t ext4 ${BOOTREC_CACHE} /cache
-busybox mount -t ext4 /dev/block/mmcblk0p10 /system
-busybox mount -t ext4 /dev/block/mmcblk0p15 /data
 
 # trigger ON amber LED
 busybox echo ${BOOTREC_RED_LED_ON} > ${BOOTREC_CONTROL_LED}
@@ -55,7 +63,7 @@ busybox echo ${BOOTREC_BUTTON_LED_OFF} > ${BOOTREC_CONTROL_LED}
 load_image=/sbin/ramdisk.cpio
 
 # boot decision
-if [ -s /dev/keycheck -o -e /cache/recovery/boot ]
+if [ -s /dev/keycheck ] || busybox grep -q recovery /cache/recovery/boot
 then
 	busybox echo 'RECOVERY BOOT' >>boot.txt
 	busybox rm -fr /cache/recovery/boot
@@ -79,16 +87,10 @@ busybox pkill -f "busybox cat ${BOOTREC_EVENT}"
 # unpack the ramdisk image
 busybox cpio -i < ${load_image}
 
-#busybox umount /cache
-#busybox umount /proc
-#busybox umount /sys
-#busybox rm -fr /dev/*
+busybox umount /cache
+busybox umount /proc
+busybox umount /sys
 
-busybox echo ${BOOTREC_CACHE_NODE} >>boot.txt
-busybox echo ${BOOTREC_EVENT_NODE} >>boot.txt
-
-busybox ls -l -R / >>boot.txt
-busybox date >>boot.txt
+busybox rm -fr /dev/*
 export PATH="${_PATH}"
 exec /init
-
